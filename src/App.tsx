e3575436c98bb5420ea0cfdd450e9873dd246a43
Gabriel Trindade <141684234+gabrieltfd2@users.noreply.github.com>
2024-06-03 23:11:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import Post from "./components/Post";
-
-import styles from './App.module.css';
-
-function App() {
-  return (
-    <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
-        Olha só que legal minha miniatura do Batmóvel.
-      </Post>
-    </div>
-  );
-}
 import React, { useState } from 'react';
 import CommentForm from './components/CommentForm';
 import CommentList from './components/CommentList';
+import Post from "./components/Post";
+import styles from './App.module.css';
 
-const App = () => {
+function App() {
   const [comments, setComments] = useState([]);
 
   const handleAddComment = (newComment) => {
@@ -23,13 +12,15 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className={styles.app}>
       <h1>Meu Aplicativo de Comentários</h1>
       <CommentForm onAddComment={handleAddComment} />
       <CommentList comments={comments} />
+      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+        Olha só que legal minha miniatura do Batmóvel.
+      </Post>
     </div>
   );
-};
+}
 
-export default App;
 export default App;
